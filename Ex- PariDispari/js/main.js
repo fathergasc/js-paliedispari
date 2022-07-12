5c10c9f8802662c3c02c164aa72c3f1a0f431323
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-12 15:21:35 +0200

    ex: PariDispari - complete

---
+++ Ex- PariDispari/js/main.js
@@ -0,0 +1,39 @@
+getValue = document.getElementById('btn');
+
+getValue.addEventListener('click', function() {
+    const number = document.getElementById('number').value;
+    console.log(`number: ${number}`);
+    const pariDispari = document.getElementById('pariDispari').value;
+    console.log(`pariDispari: ${pariDispari}`);
+    const randomNumber = generateRandomNumber(1, 5);
+    console.log('Il numero random è ' + randomNumber);
+    const sum = parseInt(number) + randomNumber;
+    const result = oddOrEven(sum);
+    alert('La somma è: ' + sum + '. Il numero è ' + result + '.');
+    if (result == 'pari' && pariDispari.value == 'even') {
+        alert('Hai vinto')     
+    } else { 
+        alert('Hai perso')
+    }
+})
+
+// FUNCTIONS
+
+//function to generate a random number
+function generateRandomNumber(min, max) {
+    if (typeof min == 'number' && typeof max == 'number') {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    } else {
+        return false;
+    }
+}
+
+//function to check if a number is odd or even
+function oddOrEven(number) {
+    if (number % 2 == 0) {
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
+}
+
